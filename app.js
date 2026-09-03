6dc600cf435075b46058f21da47543139351a634
FeiveliZsigma <feivelsubin19@gmail.com>
2026-09-03 15:01:57 -0600

    Implement navigation between welcome and main pages
    Added event listeners for main page and back button to toggle visibility.

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
+const welcomePage =
+    document.getElementById("welcomePage");
 
+const mainPage =
+    document.getElementById("mainPage");
+
+
+/* =========================
+   MAIN PAGE BUTTON
+========================= */
+
+document
+    .getElementById("mainPageButton")
+    .addEventListener("click", function () {
+
+        welcomePage.classList.remove("active");
+
+        mainPage.classList.add("active");
+
+        window.scrollTo(0, 0);
+
+    });
+
+
+/* =========================
+   BACK BUTTON
+========================= */
+
+document
+    .getElementById("backButton")
+    .addEventListener("click", function () {
+
+        mainPage.classList.remove("active");
+
+        welcomePage.classList.add("active");
+
+        window.scrollTo(0, 0);
+
+    });
